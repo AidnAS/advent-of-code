94c50967d0e954c72fc21b9f414e37665def3770
kent-remi-gabrielsen <kent-remi.gabrielsen@aidn.no>
2023-12-03 17:04:42 +0100

    Gold-star or no-star commit

--- 2023/kentremi/src/days/day1.ts
+++ 2023/kentremi/src/days/day1.ts
@@ -5,17 +5,26 @@ async function day1(task: number, dayFileHandle: FileHandle) {
   let result = 0;
   for await (const line of dayFileHandle.readLines()) {
     const matchEx =
-      task === 1 ? /\d/g : /(one|two|three|four|five|six|seven|eight|nine|\d)/g;
+      task === 1
+        ? /\d/g
+        : /(?=(one|two|three|four|five|six|seven|eight|nine|\d))/g;
 
-    const numbersInString = line.match(matchEx);
+    const numbersInString = [];
 
-    if (numbersInString) {
-      const numberString =
-        String(wordToNumber(numbersInString.slice(0)[0])) +
-        String(wordToNumber(numbersInString.slice(-1)[0]));
+    let match;
+    while ((match = matchEx.exec(line)) !== null) {
+      if (match.index === matchEx.lastIndex) {
+        matchEx.lastIndex++;
+      }
 
-      result += parseInt(numberString);
+      numbersInString.push(task === 1 ? match[0] : match[1]);
     }
+
+    const numberString =
+      String(wordToNumber(numbersInString.slice(0)[0])) +
+      String(wordToNumber(numbersInString.slice(-1)[0]));
+
+    result += parseInt(numberString);
   }
 
   return result.toString();
