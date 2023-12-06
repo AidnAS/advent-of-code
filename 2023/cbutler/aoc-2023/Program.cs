ee4b61dd8784d74805226108af2010c2dfb4e3b9
christopher <christopher.harold.butler@gmail.com>
2023-12-06 20:53:13 +0100

    get in the ring

---
+++ 2023/cbutler/aoc-2023/Program.cs
@@ -0,0 +1,34 @@
+﻿using aoc_2023.Puzzles;
+
+namespace aoc_2023;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var date = "05-12-2023"; // DateTime.Now.ToString("dd-MM-yyyy"); // "01-12-2023";
+        var useTestData = false;
+
+        Console.WriteLine($"AOC: {date}");
+        Console.WriteLine("---------------");
+        Console.WriteLine("");
+
+        // Dec01.SolvePt1(date, useTestData);
+        // Dec01.SolvePt2(date, useTestData);
+
+        // Dec02.SolvePt1(date, useTestData);
+        // Dec02.SolvePt2(date, useTestData);
+
+        // Dec03.SolvePt1(date, useTestData);
+        // Dec03.SolvePt2(date, useTestData);
+
+        // Dec04.SolvePt1(date, useTestData);
+        // Dec04.SolvePt2(date, useTestData);
+
+        // Dec05.SolvePt1(date, useTestData);
+        Dec05.SolvePt2(date, useTestData);
+
+        Console.ReadLine();
+    }
+}
+
