6d2b4d679c7103fcc69f0ac303848a2bc8acaa41
Asgeir Berland <asgeir.berland@gmail.com>
2023-12-06 11:45:51 +0100

    day 05 yolo bruteforce (for now?)

--- 2023/Trollgeir/src/main.rs
+++ 2023/Trollgeir/src/main.rs
@@ -48,7 +48,7 @@ fn get_day_solver(day: u8) -> fn() -> SolutionPair {
         2 => day02::solve,
         3 => day03::solve,
         4 => day04::solve,
-        // 5 => day05::solve,
+        5 => day05::solve,
         // 6 => day06::solve,
         //  7 => day07::solve,
         //  8 => day08::solve,
