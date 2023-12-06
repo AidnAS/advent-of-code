c0ead1e095b290cf4940ef7f87495e3ee721a2b3
Tamas Kozma <tamas-kozma@outlook.com>
2023-12-06 22:50:33 +0100

    Tamas day 6

---
+++ 2023/Tamas/Day06WaitForIt/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace Day06WaitForIt;
+
+internal static class Calculator
+{
+    public static long FindNumberOfWaysToWin(
+        Race race)
+    {
+        double underSqrt = Math.Pow(race.TimeLimit, 2) - 4 * race.RecordDistance;
+        if (underSqrt <= 0)
+        {
+            return 0;
+        }
+
+        double sqrt = Math.Sqrt(underSqrt);
+        double mind = (race.TimeLimit - sqrt) / 2;
+        double maxd = (race.TimeLimit + sqrt) / 2;
+        double mindCeiling = Math.Ceiling(mind);
+        double maxdFloor = Math.Floor(maxd);
+        // Fishy equality check with doubles :s
+        long min = (long)mindCeiling + (mind == mindCeiling ? 1 : 0);
+        long max = (long)maxdFloor - (maxd == maxdFloor ? 1 : 0);
+        if (min >= max)
+        {
+            return 0;
+        }
+
+        return max - min + 1;
+    }
+}
---
+++ 2023/Tamas/Day06WaitForIt/Program.cs
@@ -0,0 +1,31 @@
+﻿using Day06WaitForIt;
+
+var allLines = File.ReadAllLines("input.txt");
+var timeParts = allLines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+var distanceParts = allLines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int raceCount = timeParts.Length - 1;
+var allRaces = new List<Race>(raceCount);
+for (int i = 0; i < raceCount; i++)
+{
+    int arrayIndex = i + 1;
+    allRaces.Add(
+        Race.Parse(
+            timeParts[arrayIndex],
+            distanceParts[arrayIndex]));
+}
+
+long answer = 1;
+foreach (var race in allRaces)
+{
+    answer *= Calculator.FindNumberOfWaysToWin(race);
+}
+
+Console.WriteLine(
+    $"The number of ways to win in each race multiplied is {answer}.");
+
+var actualRace = Race.Parse(
+    string.Concat(timeParts.Skip(1)),
+    string.Concat(distanceParts.Skip(1)));
+long numberOfWaysToWin = Calculator.FindNumberOfWaysToWin(actualRace);
+Console.WriteLine(
+    $"The number of ways to win the one race is {numberOfWaysToWin}.");
