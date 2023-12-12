1b96989f98fbdf9bc145dea5c179f2f02be528f5
Tamas Kozma <tamas-kozma@outlook.com>
2023-12-12 23:20:44 +0100

    Tamas day 10

---
+++ 2023/Tamas/Day10PipeMaze/Program.cs
@@ -0,0 +1,8 @@
+﻿using Day10PipeMaze;
+
+var map = PipeMap.LoadFrom("input.txt");
+int distance = map.GetDistanceToFarthestLoopPoint();
+Console.WriteLine($"The farthest pipe section is {distance} steps away.");
+
+int area = map.CalculateLoopInsideArea();
+Console.WriteLine($"The inside of the loop counts {area} tiles.");
