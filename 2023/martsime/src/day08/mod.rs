e3ed6258f58153900f39fd0091907e9d5fdcf838
Martin Simensen <martsime@users.noreply.github.com>
2023-12-12 20:37:08 +0100

    Improve day 8

--- 2023/martsime/src/day08/mod.rs
+++ 2023/martsime/src/day08/mod.rs
@@ -1,42 +1,44 @@
-use std::collections::HashMap;
-use std::hash::Hash;
-use std::hash::Hasher;
-
 use crate::solution::Solution;
 
 use num::integer::lcm;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-#[derive(Default, Debug, Clone, Copy, Eq)]
+// Assumes key range [0-9A-Z]
+const KEY_LENGTH: usize = 3;
+const KEY_RANGE: usize = 36;
+const BUFFER_SIZE: usize = KEY_RANGE.pow(KEY_LENGTH as u32);
+
+#[derive(Clone, Copy, Default)]
 struct Node {
-    bytes: [u8; 3],
+    bytes: [u8; KEY_LENGTH],
     index: u16,
-}
-
-impl Hash for Node {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.bytes.hash(state);
-    }
-}
-
-impl PartialEq for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.bytes == other.bytes
-    }
 }
 
 impl Node {
     #[inline]
     fn new(bytes: &[u8]) -> Self {
+        let bytes: [u8; KEY_LENGTH] = bytes.try_into().unwrap();
         Self {
-            bytes: bytes.try_into().unwrap(),
-            index: 0,
+            bytes,
+            index: calculate_index(bytes) as u16,
         }
     }
 }
 
-#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[inline(always)]
+fn calculate_index(bytes: [u8; KEY_LENGTH]) -> usize {
+    bytes
+        .iter()
+        .enumerate()
+        .map(|(i, b)| {
+            let b = if *b <= b'9' { b - b'0' } else { b - b'A' + 10 };
+            b as usize * KEY_RANGE.pow((KEY_LENGTH - i - 1) as u32)
+        })
+        .sum()
+}
+
+#[derive(Clone, Copy, Default)]
 struct Connection {
-    from: Node,
     left: Node,
     right: Node,
 }
@@ -46,47 +48,16 @@ fn solve_part01(input: &[u8]) -> u32 {
     let directions = line_iter.next().unwrap();
     line_iter.next(); // Skip empty line
 
-    let unindexed_connections = line_iter
-        .map(|line| {
-            let from = Node::new(&line[0..3]);
-            let left = Node::new(&line[7..10]);
-            let right = Node::new(&line[12..15]);
-            Connection { from, left, right }
-        })
-        .collect::<Vec<Connection>>();
+    let mut connections: [Connection; BUFFER_SIZE] = [Default::default(); BUFFER_SIZE];
 
-    // Map all nodes to an index so that the traversing is faster
-    let mut nodes_map: HashMap<Node, u16> = HashMap::with_capacity(unindexed_connections.len());
-    let mut connections = vec![Default::default(); unindexed_connections.len()];
-    let mut current_node = Node::default();
-    let mut next_index = 0;
-    for mut connection in unindexed_connections {
-        let from_index = *nodes_map.entry(connection.from).or_insert_with(|| {
-            let index = next_index;
-            next_index += 1;
-            index
-        });
-        connection.from.index = from_index;
-        let left_index = *nodes_map.entry(connection.left).or_insert_with(|| {
-            let index = next_index;
-            next_index += 1;
-            index
-        });
-        connection.left.index = left_index;
-        let right_index = *nodes_map.entry(connection.right).or_insert_with(|| {
-            let index = next_index;
-            next_index += 1;
-            index
-        });
-        connection.right.index = right_index;
+    line_iter.for_each(|line| {
+        let from = Node::new(&line[0..3]);
+        let left = Node::new(&line[7..10]);
+        let right = Node::new(&line[12..15]);
+        connections[from.index as usize] = Connection { left, right };
+    });
 
-        if connection.from.bytes == [b'A', b'A', b'A'] {
-            current_node = connection.from;
-        }
-
-        connections[from_index as usize] = connection;
-    }
-
+    let mut current_node = Node::new(&[b'A', b'A', b'A']);
     let mut steps = 0;
 
     'outer: loop {
@@ -112,66 +83,44 @@ fn solve_part02(input: &[u8]) -> u64 {
     let directions = line_iter.next().unwrap();
     line_iter.next(); // Skip empty line
 
-    let unindexed_connections = line_iter
-        .map(|line| {
+    let mut connections: [Connection; BUFFER_SIZE] = [Default::default(); BUFFER_SIZE];
+
+    let start_nodes = line_iter
+        .filter_map(|line| {
             let from = Node::new(&line[0..3]);
             let left = Node::new(&line[7..10]);
             let right = Node::new(&line[12..15]);
-            Connection { from, left, right }
+            connections[from.index as usize] = Connection { left, right };
+
+            if from.bytes[2] == b'A' {
+                Some(from)
+            } else {
+                None
+            }
         })
-        .collect::<Vec<Connection>>();
+        .collect::<Vec<Node>>();
 
-    // Map all nodes to an index so that the traversing is faster
-    let mut nodes_map: HashMap<Node, u16> = HashMap::with_capacity(unindexed_connections.len());
-    let mut connections = vec![Default::default(); unindexed_connections.len()];
-    let mut current_nodes: Vec<(Node, u64)> = Vec::with_capacity(10);
-    let mut next_index = 0;
-    for mut connection in unindexed_connections {
-        let from_index = *nodes_map.entry(connection.from).or_insert_with(|| {
-            let index = next_index;
-            next_index += 1;
-            index
-        });
-        connection.from.index = from_index;
-        let left_index = *nodes_map.entry(connection.left).or_insert_with(|| {
-            let index = next_index;
-            next_index += 1;
-            index
-        });
-        connection.left.index = left_index;
-        let right_index = *nodes_map.entry(connection.right).or_insert_with(|| {
-            let index = next_index;
-            next_index += 1;
-            index
-        });
-        connection.right.index = right_index;
-
-        if connection.from.bytes[2] == b'A' {
-            current_nodes.push((connection.from, 0));
-        }
-
-        connections[from_index as usize] = connection;
-    }
-
-    for (current_node, steps) in current_nodes.iter_mut() {
-        'outer: loop {
-            for dir in directions {
-                *steps += 1;
-                *current_node = match dir {
-                    b'L' => connections[current_node.index as usize].left,
-                    b'R' => connections[current_node.index as usize].right,
-                    _ => unreachable!(),
-                };
-                if current_node.bytes[2] == b'Z' {
-                    break 'outer;
+    start_nodes
+        .par_iter()
+        .map(|start_node| {
+            let mut current_node = start_node;
+            let mut steps = 0;
+            'outer: loop {
+                for dir in directions {
+                    steps += 1;
+                    current_node = match dir {
+                        b'L' => &connections[current_node.index as usize].left,
+                        b'R' => &connections[current_node.index as usize].right,
+                        _ => unreachable!(),
+                    };
+                    if current_node.bytes[2] == b'Z' {
+                        break 'outer;
+                    }
                 }
             }
-        }
-    }
-
-    current_nodes
-        .iter()
-        .fold(1, |multiple, &(_, steps)| lcm(multiple, steps))
+            steps
+        })
+        .reduce(|| 1, lcm)
 }
 
 pub fn part01() -> Solution {
@@ -200,5 +149,17 @@ mod tests {
     fn day08_part02() {
         let input = include_bytes!("input_test03.txt");
         assert_eq!(solve_part02(input), 6);
+    }
+
+    #[test]
+    fn test_calculate_index() {
+        assert_eq!(calculate_index([b'0', b'0', b'0']), 0);
+        assert_eq!(calculate_index([b'0', b'0', b'1']), 1);
+        assert_eq!(
+            calculate_index([b'A', b'A', b'B']),
+            36 * 36 * 10 + 36 * 10 + 11
+        );
+        assert_eq!(calculate_index([b'Z', b'Z', b'Z']), 36 * 36 * 36 - 1);
+        assert_eq!(calculate_index([b'Z', b'Z', b'Z']), 36 * 36 * 36 - 1);
     }
 }
