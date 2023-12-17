7d4969b987cc8522ccdcf8da8c31e7ba52621499
Martin Simensen <martsime@users.noreply.github.com>
2023-12-17 12:31:48 +0100

    Improve day 17

--- 2023/martsime/src/day17/mod.rs
+++ 2023/martsime/src/day17/mod.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
+use std::collections::BinaryHeap;
 
 use crate::solution::Solution;
 
-type IndexType = i32;
-type Cost = u32;
+type IndexType = i16;
+type Cost = i32;
 type Position = (IndexType, IndexType);
 type Direction = (IndexType, IndexType);
 type Ray = (Position, Direction);
@@ -40,6 +40,54 @@ impl<'a> Board<'a> {
     }
 }
 
+#[allow(dead_code)]
+struct DistanceCache {
+    distances: Vec<Cost>,
+    width: usize,
+    height: usize,
+    size: usize,
+}
+
+impl DistanceCache {
+    pub fn new(board: &Board) -> Self {
+        let width = board.width as usize;
+        let height = board.height as usize;
+        let size = width * height;
+        let directions = 2;
+        let distances = vec![1_000_000; size * directions];
+
+        Self {
+            distances,
+            width,
+            height,
+            size,
+        }
+    }
+
+    #[inline]
+    pub fn get(&self, key: (Position, Direction)) -> Cost {
+        self.distances[self.index(key)]
+    }
+
+    #[inline]
+    fn index(&self, key: (Position, Direction)) -> usize {
+        let (position, direction) = key;
+        let (x, y) = position;
+        let dir_index = match direction {
+            UP | DOWN => 0,
+            RIGHT | LEFT => 1,
+            _ => unreachable!(),
+        };
+        dir_index * self.size + (y as usize * self.width + x as usize)
+    }
+
+    #[inline]
+    pub fn get_mut(&mut self, key: (Position, Direction)) -> &mut Cost {
+        let index = self.index(key);
+        &mut self.distances[index]
+    }
+}
+
 fn shortest_path(
     board: &Board,
     start: Position,
@@ -47,17 +95,17 @@ fn shortest_path(
     min_steps: IndexType,
     max_steps: IndexType,
 ) -> Cost {
-    let mut distances: HashMap<(Position, Direction), Cost> = HashMap::new();
-    let mut queue: Vec<Node> = vec![(0, (start, (0, 0)))];
+    let mut distances = DistanceCache::new(board);
+    let start_node = (0, (start, (0, 0)));
+    let mut queue: BinaryHeap<Node> = BinaryHeap::with_capacity(100);
+    queue.push(start_node);
     while let Some((cost, (position, previous_direction))) = queue.pop() {
+        let (x, y) = position;
         if position == end {
-            return cost;
+            return -cost;
         }
-
-        if let Some(&cheapest_cost) = distances.get(&(position, previous_direction)) {
-            if cheapest_cost < cost {
-                continue;
-            }
+        if previous_direction != (0, 0) && distances.get((position, previous_direction)) < -cost {
+            continue;
         }
 
         for (dx, dy) in DIRECTIONS {
@@ -65,10 +113,9 @@ fn shortest_path(
                 continue;
             }
 
-            let mut next_cost = cost;
+            let mut next_cost = -cost;
 
             for length in 1..=max_steps {
-                let (x, y) = position;
                 let (nx, ny) = (x + dx * length, y + dy * length);
                 if nx < 0 || nx >= board.width || ny < 0 || ny >= board.height {
                     break;
@@ -77,27 +124,30 @@ fn shortest_path(
                 next_cost += board.get(nx, ny) as Cost;
 
                 let key = ((nx, ny), (dx, dy));
-                if length >= min_steps && next_cost < *distances.get(&key).unwrap_or(&1_000_000) {
-                    distances.insert(key, next_cost);
-                    queue.push((next_cost, key));
+                let shortest_distance = distances.get_mut(key);
+                if length >= min_steps && next_cost < *shortest_distance {
+                    *shortest_distance = next_cost;
+                    queue.push((-next_cost, key));
                 }
             }
         }
-
-        queue.sort_by(|a, b| b.0.cmp(&a.0));
     }
 
     unreachable!()
 }
 
-fn solve_part01(input: &[u8]) -> u32 {
+fn solve_part01(input: &[u8]) -> i32 {
     let board = Board::parse(input);
-    shortest_path(&board, (0, 0), (board.width - 1, board.height - 1), 1, 3)
+    let start = (0, 0);
+    let end = (board.width - 1, board.height - 1);
+    shortest_path(&board, start, end, 1, 3)
 }
 
-fn solve_part02(input: &[u8]) -> u32 {
+fn solve_part02(input: &[u8]) -> i32 {
     let board = Board::parse(input);
-    shortest_path(&board, (0, 0), (board.width - 1, board.height - 1), 4, 10)
+    let start = (0, 0);
+    let end = (board.width - 1, board.height - 1);
+    shortest_path(&board, start, end, 4, 10)
 }
 
 pub fn part01() -> Solution {
